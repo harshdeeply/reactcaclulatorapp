b58744c910a95c9c7372ddfa3cc6a2e65af5239c
harshdeeply <harshdeepsingh9262@yahoo.com>
2020-10-06 14:59:19 -0700

    Initial Commit

---
+++ src/layout/Calculator/Screen/ComputationScreen/ComputationScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+const computationScreen = (props) => (
+  <div className="computation-screen">{props.children}</div>
+);
+export default computationScreen;
---
+++ src/layout/Calculator/Screen/ResultScreen/ResultScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+const resultScreen = (props) => (
+  <div className="result-screen">{props.children}</div>
+);
+export default resultScreen;
